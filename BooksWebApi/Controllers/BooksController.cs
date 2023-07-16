9d5257ddaccaa8c5cc839b0d4ba61937a0ba7612
Iucha Megreladze <imegreladze.im@gmail.com>
2023-07-16 23:13:13 +0400

    Add project files.

---
+++ BooksWebApi/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BooksWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+
+        [HttpPost("create-book")]
+        public IActionResult CreateBook(Book book)
+        {
+            MockDb.Books.Add(book.Id, book);
+            return Ok(book);
+        }
+
+
+        [HttpPut("update-book")]
+        public IActionResult UpdateBook(Book book)
+        {
+            MockDb.Books.TryGetValue(book.Id, out var existingBook);
+            if(existingBook != null)
+            {
+                existingBook.Name = book.Name;
+                existingBook.Author = book.Author;
+                return Ok(book);
+            }
+            return NotFound();
+
+        }
+
+
+        [HttpGet("book/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            MockDb.Books.TryGetValue(id, out var book);
+            return book == null ? NotFound() : Ok(book);
+        }
+
+
+        [HttpGet("books")]
+        public IActionResult GetAllBooks()
+        {
+            List<Book> books = MockDb.Books.Values.ToList();
+            return Ok(books);
+        }
+
+
+        [HttpDelete("book/{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            return MockDb.Books.Remove(id) ? Ok() : BadRequest();
+        }
+    }
+}
---
+++ BooksWebApi/MockDb.cs
@@ -0,0 +1,12 @@
+﻿namespace BooksWebApi
+{
+    public static class MockDb
+    {
+        public static Dictionary<int, Book> Books { get; set; }
+
+        static MockDb()
+        {
+            Books = new Dictionary<int, Book>();
+        }
+    }
+}
